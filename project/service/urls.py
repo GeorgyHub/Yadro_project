227166585d7d5d65b76c59e73188b8a47bc514ce
GeorgyHub <georgy.polkanov@yandex.ru>
2025-06-28 17:55:39 +0300

    Edit templates

--- project/service/urls.py
+++ project/service/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import index
+from .views import index, about, blog, performer, contacts
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about', about, name='about'),
+    path('blog', blog, name='blog'),
+    path('performer', performer, name="performer"),
+    path('contact', contacts, name="contacts"),
 ]
--- project/service/views.py
+++ project/service/views.py
@@ -3,4 +3,31 @@ from rest_framework import routers, serializers, viewsets
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, 'service/index.html', context=context)
+
+def about(request):
+    context = {
+        'title': 'О нас'
+    }
+    return render(request, 'service/about.html', context=context)
+
+def blog(request):
+    context = {
+        'title': 'Блог'
+    }
+    return render(request, 'service/blog.html', context=context)
+
+def performer(request):
+    context = {
+        'title': 'Исполнитель'
+    }
+    return render(request, 'service/performer.html', context=context)
+
+def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'service/contact.html', context=context)
